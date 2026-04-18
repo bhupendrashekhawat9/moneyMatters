219601c9e78bbdd00e0b759388d6e7df10f868c7
bhupendrashekhawat9 <bistshekhawat@gmail.com>
2026-04-18 10:31:03 +0530

    chore: add keep alive server code

--- src/server.ts
+++ src/server.ts
@@ -12,4 +12,17 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
   console.log(`API Documentation: http://localhost:${PORT}/api-docs`);
+  setInterval(() => {
+    fetch(`${process.env.PROD_URL}/`,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      keepalive: true,
+      
+    }).catch(() => {
+      console.error('Failed to fetch API documentation');
+    });
+
+  }, 40000); // Log every 50 seconds to keep the server alive
 });
